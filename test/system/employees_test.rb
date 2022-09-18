d21345d77fc349f45e86289619b8468ddf74351d
Chad Tarpey <chadtarpey@gmail.com>
2022-09-18 17:32:09 -0500

    added the system test for employees

--- test/system/employees_test.rb
+++ test/system/employees_test.rb
@@ -3,6 +3,7 @@ require "application_system_test_case"
 class EmployeesTest < ApplicationSystemTestCase
   setup do
     @employee = employees(:one)
+    @employee = employees(:two)
   end
 
   test "visiting the index" do
@@ -11,37 +12,61 @@ class EmployeesTest < ApplicationSystemTestCase
   end
 
   test "should create employee" do
+    sign_in users(:two)
     visit employees_url
-    click_on "New employee"
-
+    click_on "Add employee"
     fill_in "Bio", with: @employee.bio
     fill_in "Email", with: @employee.email
     fill_in "Name", with: @employee.name
-    fill_in "Users", with: @employee.users_id
     click_on "Create Employee"
-
     assert_text "Employee was successfully created"
     click_on "Back"
   end
 
   test "should update Employee" do
+    sign_in users(:two)
     visit employee_url(@employee)
-    click_on "Edit this employee", match: :first
-
+    click_on "Edit", match: :first
     fill_in "Bio", with: @employee.bio
     fill_in "Email", with: @employee.email
     fill_in "Name", with: @employee.name
-    fill_in "Users", with: @employee.users_id
     click_on "Update Employee"
-
     assert_text "Employee was successfully updated"
     click_on "Back"
   end
 
   test "should destroy Employee" do
+    sign_in users(:two)
     visit employee_url(@employee)
     click_on "Destroy this employee", match: :first
-
     assert_text "Employee was successfully destroyed"
   end
+
+  test "should search for employee" do 
+    sign_in users(:one)
+    visit employees_url
+    fill_in "search", with: "Brandon"
+    click_on "Search"
+      # within "table" do
+      # assert_text "Brandon Goodman"
+      # end 
+    assert page.has_content?("Brandon Goodman")  
+  end
+
+
+  test "should click on next & prev for pagination" do 
+    sign_in users(:one)
+    visit employees_url
+    click_on 'Next ›'
+    click_on '‹ Prev'
+  end
+
+  test "user should log out successfully" do 
+    sign_in users(:two)
+    visit employees_url 
+    click_on 'Logout'
+    assert page.has_content? 'Signed out successfully.' 
+  end
+
 end
+
